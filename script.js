bfb235a0f89486140e6d9f552081ffed8a12e040
Uy Binh <uybinh@gmail.com>
2018-05-16 06:32:27 +0700

    Create multiple random circles with random color

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+var circles = [];
+
 function onKeyDown(event){
   var maxPoint = new Point(view.size.width, view.size.height);
   var randomPoint = new Point.random();
   var point = maxPoint * randomPoint;
-  new Path.Circle(point, 10).fillColor = "green";
+  var circle = new Path.Circle(point, 500);
+  circle.fillColor = "yellow";
+  circles.push(circle);
 }
+
+function onFrame(event){
+  for(var i = 0; i < circles.length; i++){
+    circles[i].fillColor.hue += 1;
+    circles[i].scale(0.9);
+  };
+}
+
